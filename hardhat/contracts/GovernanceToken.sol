cedda7b85a5c295f0fc23f95f0f5f7e60568e398
Zeeshan Qureshi <pmz5164300@gmail.com>
2026-03-07 22:49:08 +0500

    Adding contract

--- hardhat/contracts/GovernanceToken.sol
+++ hardhat/contracts/GovernanceToken.sol
@@ -1,14 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
+/**
+* @title   ERC20 token for Governance
+* @author  Zeeshan (github/shanppc)
+*/
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract PGT is ERC20 {
     constructor(uint256 initialSupply) ERC20("PolkaGrant Token", "PGT") {
         _mint(msg.sender, initialSupply);
     }
-    function decimals() public view virtual override returns (uint8) {
-  return 18;
-} 
 
 }
